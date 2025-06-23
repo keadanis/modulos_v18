3d1b93fb58055e72e1e63a86a7a9437ff38d870b
keadanis <keadanis@hotmail.com>
2025-06-23 11:24:01 -0600

    ADD original module

---
+++ se_server_management/models/command.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class CommandCommand(models.Model):
+    _name = "server.command"
+    _description = "command"
+    _order = "sequence"
+
+    sequence = fields.Integer(
+        string="sequence",
+        default=10,
+    )
+    name = fields.Char(string="Name", required=True)
+    command = fields.Text(string="Command", required=True)
+
+    server_id = fields.Many2one(comodel_name="server.server", string="Server")
+
+    def action_ejecutar(self):
+        for record in self:
+            if record.server_id:
+                record.server_id.custom_sudo(self.command)
